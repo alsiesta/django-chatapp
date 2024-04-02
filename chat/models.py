e83e2786fea29cd8425e43f5f30c6c03e4cbc817
alsiesta <as@daslizenzatelier.de>
2024-04-02 19:44:42 +0200

    created first model and db

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+
+# class Chat(models.Model):
+#     created_at = models.DateTimeField(default=date.today)
+    
+
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = models.DateTimeField(default=date.today)
+    # chat = Chat Klasse verknüpfen
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_messages_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_messages_set')
