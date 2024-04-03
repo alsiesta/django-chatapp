fa7cd1b7cd7f4c467285ff3a66271e298c24c592
alsiesta <as@daslizenzatelier.de>
2024-04-03 16:06:28 +0200

    added login functionality

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,20 @@
 # pylint: disable=no-member
 
 from django.shortcuts import render
+
 from chat.models import Message
+
 from chat.models import Chat
 
+from django.contrib.auth import authenticate
 
+from django.contrib.auth import login
+
+from django.http import HttpResponseRedirect
+
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
 def index(request):
     print(request.method)
     if request.method == 'POST':
@@ -17,3 +27,17 @@ def index(request):
     print(chat_messages)
         
     return render(request, 'chat/index.html', {'chat_messages': chat_messages})
+
+def login_view(request):
+    print("THE GET REQUEST: ",request.GET)
+    redirect= request.GET.get('next')
+    
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
