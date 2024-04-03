2be0324d5b7da133478a0b706eb9f8ecc28c1961
alsiesta <as@daslizenzatelier.de>
2024-04-03 13:33:20 +0200

    created authentication

--- chat/views.py
+++ chat/views.py
@@ -10,6 +10,10 @@ def index(request):
     if request.method == 'POST':
         print('Received Data: ' + request.POST['textmessage'])
         myChat=Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
         
-    return render(request, 'chat/index.html', {'name': 'Alexander'})
+    chat_messages = Message.objects.filter(chat__id=1)
+    # chat_messages = Message.objects.filter(created_at='2024-04-02')
+    print(chat_messages)
+        
+    return render(request, 'chat/index.html', {'chat_messages': chat_messages})
