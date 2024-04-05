1a9bfa63faa19017764de65670f7b8bb10dce7e6
alsiesta <as@daslizenzatelier.de>
2024-04-05 18:38:44 +0200

    styled Login

--- chat/models.py
+++ chat/models.py
@@ -11,6 +11,6 @@ class Chat(models.Model):
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_messages_set', default=None, blank=True, null=True) # standardwer is None, filed darf leer sein und die DB darf null sein
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_messages_set', default=None, blank=True, null=True) # default is None, feld darf leer sein und die DB darf null sein
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_messages_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_messages_set')
