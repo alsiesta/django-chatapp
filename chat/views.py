ab13252c65429ba9abccef9279b5d99a935d7448
alsiesta <as@daslizenzatelier.de>
2024-04-12 16:31:12 +0200

    finished styling

--- chat/views.py
+++ chat/views.py
@@ -1,30 +1,31 @@
-# pylint: disable=no-member
+# pylint: disable=no-member, disable=missing-function-docstring,invalid-name
+
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+from django.core import serializers
+
 from chat.models import Message
 from chat.models import Chat
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-
 
 @login_required(login_url='/login/')
 def index(request):
-    print(request.method)
     if request.method == 'POST':
         print('Received Data: ' + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(
-            text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [ new_message, ])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+    
     chat_messages = Message.objects.filter(chat__id=1)
-    # chat_messages = Message.objects.filter(created_at='2024-04-02')
-    print(chat_messages)
-
-    return render(request, 'chat/index.html', {'chat_messages': chat_messages})
+    last_message_date = chat_messages.last().created_at
+    return render(request, 'chat/index.html', {'chat_messages': chat_messages, 'last_message_date': last_message_date})
 
 
 def login_view(request):
@@ -43,9 +44,9 @@ def login_view(request):
 
 
 def register_view(request):
- 
+
     if request.method == 'POST':
-        print("REGISTER REQUEST POST: ",request.POST)
+        print("REGISTER REQUEST POST: ", request.POST)
         username = request.POST['username']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
@@ -60,8 +61,9 @@ def register_view(request):
 
         User.objects.create_user(username=username, password=password)
         return redirect('/chat/')
-    
+
     return render(request, 'auth/register.html')
+
 
 def logout_view(request):
     logout(request)
